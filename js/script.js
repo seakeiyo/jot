60befab0e730e8c4bf865e4bd7978d182aa2c13a
seakeiyo <seakeiyo@gmail.com>
2014-10-09 20:08:52 +0800

    changed save interval to 500ms

--- js/script.js
+++ js/script.js
@@ -1,25 +1,6 @@
 (function (d, w) {
 	var html = d.documentElement,
 		body = d.body,
-		
-		titleField1 = $('#title-1'),
-		noteTextArea = $('#note-1');
-		
-	/*$(titleField1).add(noteTextArea).on('change keyup paste', function (e) {
-		if(typeof(s) != 'undefined') clearTimeout(s);
-		
-		s = setTimeout(function(){		
-				$.post('php/save.php', {
-					titleFieldID: titleField1.attr('id'),
-					title: titleField1.val(),
-					content: noteTextArea.val()
-				}, function (data) {					
-					console.log(data);
-				});
-		
-				console.log("saved");
-			},1000);
-	});*/
 	
 	$('.bind-evt').on('change keyup paste', function (e) {
 		var isTitle = $(this).hasClass('bind-evt-title'),
@@ -48,8 +29,6 @@
 				});
 		
 				console.log("saved");
-			},1000);
-		
-		//	console.log('Wawa ' + $(this).attr('id') + ' ' + $(counterpart).attr('id'));
+			},500);
 	});
 })(document, window);
